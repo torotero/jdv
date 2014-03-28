0ad35c78f9ecb53fbf2d70b2798134d6bc3bd1ad
torotero <ganza.toro@gmail.com>
2014-03-28 10:26:56 -0400

    change describe and updates it do

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-		describe "creating todo lists" do
+		describe "Editing todo lists" do
 			It "updates a todo list successfully with correct information"
 			visit "/todo_lists"
 		end
