8d897654877f5c6fdad54e6a5d95bae6a26414a9
torotero <ganza.toro@gmail.com>
2014-03-28 10:44:52 -0400

    add first testing from #x to #y

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -2,6 +2,29 @@ require 'spec_helper'
 
 		describe "Editing todo lists" do
 			It "updates a todo list successfully with correct information" do
+
+			todo_lists = TodoList.create(title:"Groceries", description: "Grocery list.")	
+
+
+
+
 			visit "/todo_lists"
+			within "#todo_list_#{todo_list.id}"
+			click_link "Edit"
+
 		end
+
+
+
+		fill_in "Title", with:"New title"
+		fill_in "Description", with:"New description"
+		click_button "Update Todo list"
+
+		expect(page).to have_content("Todo list was successfully updated.")
+		expect(todo_list.title).to eq("New title")
+		expect()
+
+
+
+
 	end
