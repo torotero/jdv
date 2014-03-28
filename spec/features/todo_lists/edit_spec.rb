150006b71a29859919c83bd1e44ae8cd456a9b2f
torotero <ganza.toro@gmail.com>
2014-03-28 10:33:51 -0400

    add dom after tr ->   dom_id(todo_list)

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 		describe "Editing todo lists" do
-			It "updates a todo list successfully with correct information"
+			It "updates a todo list successfully with correct information" do
 			visit "/todo_lists"
 		end
 	end
